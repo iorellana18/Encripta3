c9264c8b9b8a7ed2d0349d635a3c98b8807f4abd
Ian <ian.orellana@usach.cl>
2016-11-16 04:51:37 -0300

    Encriptado

--- src/encripta3/Encripta3.java
+++ src/encripta3/Encripta3.java
@@ -14,27 +14,70 @@ import java.io.IOException;
  * @author ian
  */
 public class Encripta3 {
-
+ 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        FileReader texto, clave;
+        FileReader file1, file2;
         BufferedReader bufferTexto, bufferClave;
-        String secuencia, pass;
+        String texto, clave;
+        String[] Bloques;
+        String[] BloquesClave;
+        String cadena ="";
         
        try{
-           texto= new FileReader("/home/ian/Escritorio/mensaje"); //args[0]
-           clave = new FileReader("/home/ian/Escritorio/clave"); //args[1]
-           bufferTexto = new BufferedReader(texto);
-           bufferClave = new BufferedReader(clave);
-           secuencia = bufferTexto.readLine();
-           pass = bufferClave.readLine();
+           file1= new FileReader("/home/ian/Escritorio/mensaje"); //args[0]
+           file2 = new FileReader("/home/ian/Escritorio/clave"); //args[1]
+           final int numBloques = 3;
+           bufferTexto = new BufferedReader(file1);
+           bufferClave = new BufferedReader(file2);
+           texto = bufferTexto.readLine();
+           clave = bufferClave.readLine();
            
            tools t = new tools();
-           t.codifica(secuencia);
+           //Obtiene bloques - texto.length/blques+1
+           int cantidadBloques=texto.length()/numBloques+1;
+           Bloques=t.cortaBlques(numBloques, texto);
+           BloquesClave=t.cortaBlques(numBloques, clave);
+           int i=1;
+           System.out.println("Texto original :"+texto);
+           System.out.println("Clave original :"+clave);
+           System.out.println("Clave codificada: "+t.ajustaClave(texto,t.codificaASCII(clave)));
+           while(i<=cantidadBloques){
+               texto=Bloques[i];
+               clave=BloquesClave[i];
+               //mesnaje
+                System.out.println("mensaje: "+texto);
+
+                //clave
+                System.out.println("clave: "+clave);
+
+                //texto codificado
+                texto = t.codifica(texto);
+                System.out.println("mesaje codificado: "+texto);
+
+                //clave ajustada
+                clave = t.ajustaClave(texto,t.codificaASCII(clave));
+                System.out.println("clave ajustada: "+clave);
+
+                //Texto sustituido
+                texto=t.Sustitucion(texto, clave);
+                System.out.println("texto sustituido: "+texto);
+                
+                texto=t.Transposicion(numBloques, texto);
+                System.out.println("texto traspuesto: "+texto);
+                cadena=cadena+texto;
+               i++;
+           }
+           System.out.println(cadena);
+           cadena=t.Interpone(cadena);
+           System.out.println(cadena);
+           t.finalEncrypt(cadena);
+          /* 
            
-           
+           */
            
        }catch (FileNotFoundException e) {
            throw new RuntimeException("Error reading file");
--- src/encripta3/tools.java
+++ src/encripta3/tools.java
@@ -10,6 +10,8 @@ package encripta3;
  * @author ian
  */
 public class tools {
+    
+    
     public String ajustaClave(String texto, String clave){
         StringBuilder nuevaClave = new StringBuilder();
         int largoClave=clave.length(), largoTexto=texto.length();
@@ -37,12 +39,13 @@ public class tools {
         return nuevaClave.reverse().toString();
     }
     
-    public void codifica(String texto){
+    public String codifica(String texto){
         int largoTexto = texto.length();
         texto = texto.toUpperCase();
         char[] arreglo = texto.toCharArray();
         int numero, i=0;
         String codigo;
+        StringBuilder cadena = new StringBuilder();
         
         while(i<largoTexto){
             numero = arreglo[i]-64;
@@ -54,14 +57,139 @@ public class tools {
                 }
                 codigo ="0";
                 codigo = codigo.concat(String.valueOf(numero));
-                System.out.println(codigo);
+                cadena.append(codigo);
                 
             }else{
                 codigo = String.valueOf(numero);
-                System.out.println(codigo);
+                cadena.append(codigo);
             }
             i++;
         }
+        return cadena.toString();
+    }
+    
+    public String codificaASCII(String texto){
+        int largoTexto = texto.length();
+        texto = texto.toUpperCase();
+        StringBuilder cadena = new StringBuilder();
+        char[] arreglo = texto.toCharArray();
+        int numero, i=0;
+        String codigo;
         
+        while(i<largoTexto){
+            numero = arreglo[i];
+            codigo = String.valueOf(numero);
+            cadena.append(codigo);
+            i++;
+        }
+        return cadena.toString();
+    }
+    
+    public String Sustitucion(String texto, String clave){
+        StringBuilder cadena = new StringBuilder(), tempTexto = new StringBuilder(), tempClave = new StringBuilder();
+        String valorStr;
+        //tienen mismo largo en este punto
+        int largo = texto.length(), i=0, valor;
+        
+        while(i<largo){
+            tempTexto.append(texto.charAt(i));
+            tempClave.append(clave.charAt(i));
+            if(tempTexto.length()==2){
+                valor=Integer.parseInt(tempTexto.toString())+Integer.parseInt(tempClave.toString());
+                valor=valor%28;
+                if(valor<10){
+                    valorStr="0";
+                    valorStr=valorStr.concat(String.valueOf(valor));
+                }else{
+                    valorStr=String.valueOf(valor);
+                }
+                cadena.append(valorStr);
+                tempTexto.delete(0, 2);
+                tempClave.delete(0, 2);
+            }
+            i++;
+        }
+        return cadena.toString();
+    }
+    
+    public String[] cortaBlques(int numBloques, String texto){
+        int largo=texto.length(), i=0;
+        String[] array;
+        String cadena="";
+        while(i<largo){
+            
+            if(i%numBloques==0){
+                cadena=cadena+"&";
+            }
+            cadena=cadena+texto.charAt(i);
+            i++;
+        }
+        
+        //System.out.println(cadena);
+        array=cadena.split("&");
+        //System.out.println(array[4]);
+        return array;
+    }
+    
+    public String Transposicion(int numBloque, String texto){
+        int largo = texto.length(), i=0,j=0;
+        String cadena="";
+        String traspuesta="";
+        while(i<numBloque){
+            while(j<largo){
+                cadena=cadena+texto.charAt(j);
+                j=j+numBloque;
+            }
+            traspuesta=traspuesta+cadena;
+            cadena="";
+            i++;
+            j=i;
+        }
+        return traspuesta;
+    }
+    
+    public String Interpone(String texto){
+        int largo = texto.length(),i=0;
+        boolean flag=true;
+        String cadena="";
+        
+        while(i<largo){
+            if(i%2==0){
+                
+                if(flag){
+                    cadena=cadena+"1";
+                    flag=false;
+                }else{
+                    cadena=cadena+"2";
+                    flag=true;
+                }
+            }
+            cadena=cadena+texto.charAt(i);
+            i++;
+        }
+        return cadena;
+        
+    }
+    
+    public void finalEncrypt(String texto){
+       int largo = texto.length(), i=0;
+       String Encrypt="",temp="";
+       int valor; char ascii;
+       while(i<largo){
+           if(i%3==0 && i!=0){
+               valor=Integer.parseInt(temp);
+               ascii=(char)valor;
+               Encrypt=Encrypt+ascii;
+               temp="";
+           }
+           temp=temp+texto.charAt(i);
+           
+           i++;
+       }
+        valor=Integer.parseInt(temp);
+        ascii=(char)valor;
+        Encrypt=Encrypt+ascii;
+        System.out.println(Encrypt+"");
+       
     }
 }
